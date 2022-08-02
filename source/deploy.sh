2236dbaf12493676a1744ce96db8cce8338bfab0
kalineh <kalin.eh@gmail.com>
2022-08-01 20:40:15 -0700

    deploy script

--- source/deploy.sh
+++ source/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm mod_scaling_avatar.zip
+rm mod_scaling_avatar*.zip
 pushd mod_scaling_avatar
-7z a mod_scaling_avatar.zip .
-rm /c/Program\ Files\ \(x86\)/Steam/steamapps/common/Battle\ Brothers/data/mod_scaling_avatar.zip
-cp ./mod_scaling_avatar.zip /c/Program\ Files\ \(x86\)/Steam/steamapps/common/Battle\ Brothers/data/mod_scaling_avatar.zip
+7z a mod_scaling_avatar-0-0-2-dev.zip .
+rm /c/Program\ Files\ \(x86\)/Steam/steamapps/common/Battle\ Brothers/data/mod_scaling_avatar*.zip
+cp ./mod_scaling_avatar.zip /c/Program\ Files\ \(x86\)/Steam/steamapps/common/Battle\ Brothers/data/mod_scaling_avatar-0-0-2-dev.zip
 popd
